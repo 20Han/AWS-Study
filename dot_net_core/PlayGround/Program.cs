283975b283cc829ca617dc715bcae410a0ef41e6
이영한 <mashiro@snu.ac.kr>
2022-04-05 23:32:24 +0900

    c# TodoAPI sample project

---
+++ dot_net_core/PlayGround/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Task<int> task = sleepFunction();
+            Console.WriteLine("Hello World!2");
+
+            int value = await task;
+            Console.WriteLine("Hello World!3");
+            Console.WriteLine("{0}", value);
+        }
+
+        private static async Task<int> sleepFunction() 
+        {
+            await Task.Delay(100);
+            Console.WriteLine("sleepFunction");
+            return 1;
+        }
+    }
+}
